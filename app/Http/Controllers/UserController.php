9a9509c081f7719c7ada68b92230a456f6ec02ab
Carles Vargas <carlesvargas5@gmail.com>
2023-04-17 20:13:54 +0200

    docs: UserController documented

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,24 +25,53 @@ class UserController extends Controller
         $this->repository = $repository;
     }
 
+    /**
+     * This function retrieves a list of customers and returns it as a collection.
+     *
+     * @return Response
+     */
     public function index()
     {
         $users = $this->repository->getCustomers();
         return $this->respondWithCollection($users);
     }
 
+    /**
+     * This PHP function retrieves a user by their ID and returns a response with the user's
+     * information.
+     *
+     * @param int id The parameter "id" is an integer that represents the unique identifier of the user
+     * that we want to retrieve from the database.
+     *
+     * @return Response
+     */
     public function show(int $id)
     {
         $user = $this->repository->find($id);
         return $this->respondWithItem($user);
     }
 
+    /**
+     * This PHP function creates a new user and returns a response with the created user's data.
+     *
+     * @param UserCreateRequest $request
+     *
+     * @return Response
+     */
     public function create(UserCreateRequest $request)
     {
         $user = $this->repository->create($request->validated());
         return $this->respondWithItem($user, 201);
     }
 
+    /**
+     * This function updates a user's information and returns a success message or throws an exception
+     * if the update fails.
+     *
+     * @param UserUpdateRequest $request
+     *
+     * @return Response
+     */
     public function update(UserUpdateRequest $request)
     {
         $updated = $this->repository->update($request->validated(), $request->input('user_id'));
@@ -53,6 +82,15 @@ class UserController extends Controller
         return $this->respondWithMessage(__('controller.user.updated'));
     }
 
+    /**
+     * This function deletes a user with a given ID and returns a success message, or throws an
+     * exception if the deletion fails.
+     *
+     * @param id The parameter `id` is the unique identifier of the record that needs to be deleted
+     * from the database. It is passed as an argument to the `delete()` function.
+     *
+     * @return Response
+     */
     public function delete($id)
     {
         $deleted = $this->repository->delete($id);
